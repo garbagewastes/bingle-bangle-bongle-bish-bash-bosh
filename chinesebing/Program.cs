19deff43d9d607cc69b80387b85430fe56103d7f
Quinn McCormick (1000141013) <MCCOQA1@student.op.ac.nz>
2026-05-12 10:45:17 +1200

    bing mmmmmm

---
+++ chinesebing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace chinesebing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
